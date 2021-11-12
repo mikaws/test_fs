aca125bc92d749146936b1e7054ea30c161108cb
Michael <michaelhenriqued@gmail.com>
2021-11-12 18:45:50 -0300

    fix loop

--- index.js
+++ index.js
@@ -2,25 +2,30 @@ let fs = require('fs');
 let http = require('http')
 const path = require("path")
 
-// const download = function(url, dest, cb) {
-//     let file = fs.createWriteStream(dest);
-//     let request = http.get(url, function(response) {
-//       response.pipe(file);
-//       file.on('finish', function() {
-//         file.end();  // close() is async, call cb after close completes.
-//       });
-//     }).on('error', function(err) { // Handle errors
-//         console.log(dest.substring(dest.lastIndexOf("/")+1))
-//     //   fs.unlink(dest.substring()); // Delete the file async. (But we don't check the result)
-//       if (cb) cb(err.message);
-//     });
-// };
+const dirPath = 'E:/calls/'
+const endpoint = 'http://192.168.1.250:6373/mamute-info.cgi?action=file&key=12345678900&id=1634928197.416059'
+const maxSize = 5.0
+const fileName = `${Math.random(10)}.gsm`
+
+function download(endpoint, filePath, callback) {
+  let file = fs.createWriteStream(filePath)
+
+  http.get(endpoint, function(response) {
+    response.pipe(file)
+    file.on('finish', function() { file.close(callback)  })
+  })
+    .on('error', function(error) {
+      // delete the file async
+      fs.unlink(filePath); 
+      if (callback)
+        cb(error.message);
+    })
+};
 
 // SIZE OF FOLDER
-const getAllFiles = function(dirPath, arrayOfFiles) {
-  files = fs.readdirSync(dirPath)
-
-  arrayOfFiles = arrayOfFiles || []
+function getAllFiles(dirPath) {
+  let files = fs.readdirSync(dirPath)
+  arrayOfFiles = []
 
   files.forEach(function(file) {
     if (fs.statSync(dirPath + "/" + file).isDirectory()) {
@@ -33,51 +38,59 @@ const getAllFiles = function(dirPath, arrayOfFiles) {
   return arrayOfFiles
 }
 
-const getTotalSize = function(directoryPath) {
-  const arrayOfFiles = getAllFiles(directoryPath)
-
+function getTotalSize(dirPath) {
+  const arrayOfFiles = getAllFiles(dirPath)
   let totalSize = 0
 
   arrayOfFiles.forEach(function(filePath) {
-    totalSize += fs.statSync(filePath).size
+    try {
+      if (fs.existsSync(filePath)) {
+        totalSize += fs.statSync(filePath).size
+      }
+    } catch (error) {
+      console.log('error')
+    }
   })
 
   return parseFloat((totalSize/1024)/1024).toFixed(4)
 }
 
-let dates = []
-
 // // TO DELETE THE OLDEST FILE
-const deleteHandler = () => {
+const deleteHandler = async () => {
+  
+    let dates = []
+    let dirSize = getTotalSize(dirPath)
+    let files = fs.readdirSync(dirPath)
+    
     //array with each string containing the birthtime and name of all files in the folder
     files.forEach(file => {
-        let date = fs.statSync('E://calls/' + file).birthtime
-        dates.push(`${date},${file}`)
-    });
+        if (file) {
+            let date = fs.statSync(dirPath + file).birthtime
+            dates.push(`${date},${file}`)
+        }
+    })
 
-    //array ordered by crescent date
+    // // array ordered by crescent date
     const orderedDates = dates.sort(function(lastDate, firstDate) {
         return Date.parse(lastDate.split(',', 1)[0]) - Date.parse(firstDate.split(',', 1)[0]);
-    });
+    })
 
-    //get the name of file which will be deleted
-    const fileToDelete = orderedDates[0].substring(orderedDates[0].lastIndexOf(",")+1)
+    // //get the name of file which will be deleted
+    const fileToDeletePath =  orderedDates[0] ? orderedDates[0].substring(orderedDates[0].lastIndexOf(",")+1) : ''
 
-    fs.unlink('E://calls/' + fileToDelete, (err) => {
-        if (err) {throw err};
-            console.log(fileToDelete + ' was deleted');
-    });
-}
-
-const downloadHandler = () => {
-    
-    // const res = download('http://192.168.1.250:6373/mamute-info.cgi?action=file&key=12345678900&id=1634928197.416059', 'E:/calls/'+ Math.random(10) + '.gsm')
-    
-    let totalSize = getTotalSize("E://calls/")
-    if (totalSize > 6.0) {
+    if (dirSize > maxSize && fileToDeletePath) {
+      fs.unlink(dirPath + fileToDeletePath, (err) => {
+        if (err)
+          throw err
+        console.log(fileToDeletePath + ' was deleted');
         deleteHandler()
+      })
     }
 }
 
+const downloadHandler = () => {
+    deleteHandler()
+    download(endpoint, dirPath + fileName)
+}
 
 downloadHandler()
